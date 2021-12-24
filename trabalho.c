bd7ba4abbeaf6b0adc8dda4519c1a8289e83d59b
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-12-24 18:31:59 -0300

    Inicialização do projeto

---
+++ trabalho.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<windows.h>
+#include<wincon.h>
+
+void inicializacao(){
+    system("cls");
+    SetConsoleTitle("Trabalho 1 - Eduardo Conde Pires");
+
+}
+
+void main(){
+    // exibição da tela inicial e outras configurações
+    inicializacao();
+}
