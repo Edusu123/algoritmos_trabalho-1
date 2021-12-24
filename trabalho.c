aae60de913ecfb21d97921141856650a43ee8333
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-12-24 18:38:00 -0300

    Movendo a função de inicialização para baixo e definindo o tamanho da janela

--- trabalho.c
+++ trabalho.c
@@ -2,13 +2,19 @@
 #include<windows.h>
 #include<wincon.h>
 
-void inicializacao(){
-    system("cls");
-    SetConsoleTitle("Trabalho 1 - Eduardo Conde Pires");
-
-}
+void inicializacao();
 
 void main(){
     // exibição da tela inicial e outras configurações
     inicializacao();
+}
+
+void inicializacao(){
+    system("cls");
+
+    // nome da janela do console
+    SetConsoleTitle("Trabalho 1 - Eduardo Conde Pires");
+    
+    // define o tamanho da tela
+    system("mode con:cols=110 lines=30");
 }
