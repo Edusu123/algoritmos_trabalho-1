7b6ee633173a79cb763c402ee5d0903f74ddac6d
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-12-24 18:41:35 -0300

    Implementando setlocale e organização do código

--- trabalho.c
+++ trabalho.c
@@ -1,8 +1,10 @@
+#include<locale.h>
 #include<stdio.h>
 #include<windows.h>
 #include<wincon.h>
 
 void inicializacao();
+void menu();
 
 void main(){
     // exibição da tela inicial e outras configurações
@@ -12,9 +14,15 @@ void main(){
 void inicializacao(){
     system("cls");
 
+    setlocale(LC_ALL, "pt_br");
+
     // nome da janela do console
     SetConsoleTitle("Trabalho 1 - Eduardo Conde Pires");
     
     // define o tamanho da tela
     system("mode con:cols=110 lines=30");
+}
+
+void menu(){
+
 }
