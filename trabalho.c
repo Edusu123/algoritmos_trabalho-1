91f60e37be25972f03d9701d5ed5e1af68bfc7a2
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2021-12-24 18:58:19 -0300

    Início da implementação do menu para escolha das ações

--- trabalho.c
+++ trabalho.c
@@ -6,14 +6,20 @@
 void inicializacao();
 void menu();
 
+void cursor(int);
+void gotoxy(int, int); // coluna, linha
+
 void main(){
     // exibição da tela inicial e outras configurações
     inicializacao();
+
+    menu();
 }
 
 void inicializacao(){
     system("cls");
 
+    // prompt passa a aceitar codifição de texto com acentos
     setlocale(LC_ALL, "pt_br");
 
     // nome da janela do console
@@ -24,5 +30,39 @@ void inicializacao(){
 }
 
 void menu(){
+    int sair = 1;
+    int pos = 10;
+    
+    do{
+        // esconde o cursor
+        cursor(0);
 
+        gotoxy(5, 10); printf("Bem-Vindo!");
+
+    }while(sair);
+}
+
+void cursor(int status){
+	switch (status) {
+		case 0: {
+			CONSOLE_CURSOR_INFO cursor = {1, FALSE};
+			SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+			break;
+		}
+		case 1: {
+			CONSOLE_CURSOR_INFO cursor = {1, TRUE};
+			SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+			break;
+		}
+	}
+}
+
+void gotoxy(int x, int y)
+{
+    COORD c;
+
+    c.X = x - 1;
+    c.Y = y - 1;
+    
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
